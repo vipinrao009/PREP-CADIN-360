80139c6614fb1b65e8356b42a38ef77d6169503c
vipinrao009 <vipinrao2018@gmail.com>
2025-08-06 01:13:21 +0530

    feat: update dependencies and integrate Tailwind CSS with DaisyUI
    - Added @reduxjs/toolkit and daisyui to dependencies
    - Updated tailwindcss and react-redux versions
    - Modified App component to include a new button styled with DaisyUI
    - Replaced Tailwind CSS import in index.css with @tailwind directives
    - Removed tailwindcss plugin from Vite configuration
    - Added postcss.config.js to configure Tailwind CSS and autoprefixer
    - Created tailwind.config.js to set up DaisyUI and specify content paths

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
      <div className='p-2 m-1 justify-center'>
         <h1 className=' bg-amber-400 p-2'>Hello react</h1>
+        <button className='btn btn-neutral'>Hello</button>
      </div>
     </>
   )
