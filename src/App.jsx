abd8c16cdea90672f456232577f7d18340e77a61
vipinrao009 <vipinrao2018@gmail.com>
2025-08-05 17:25:18 +0530

    feat: integrate Tailwind CSS into the project
    - Added Tailwind CSS and its Vite plugin as dependencies in package.json
    - Updated App component to use Tailwind CSS classes for styling
    - Created index.css to import Tailwind CSS styles
    - Configured Vite to use Tailwind CSS plugin

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <>
-    <h1>Hello react</h1>
+     <div className='p-2 m-1 justify-center'>
+        <h1 className=' bg-amber-400 p-2'>Hello react</h1>
+     </div>
     </>
   )
 }
